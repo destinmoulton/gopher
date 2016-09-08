b5acc0c7daa8bc9ff2ec6c0c9d7b644968830ee5
Destin Moulton <destin.moulton@gmail.com>
2016-09-08 11:27:46 -0600

    Added basic mocha test file.

---
+++ test/gopher.js
@@ -0,0 +1,10 @@
+var expect = require("chai").expect;
+
+describe("A pub/sub message/package broadcast system", function(){
+    describe("Subscribe functions for later calling via broadcast", function(){
+        it("subscribes a function and broadcasts a package to that function", function(){
+            
+        });
+
+    });
+});
