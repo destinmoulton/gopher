830ba586667169c96d9d8a975c15e2fc65396611
Destin Moulton <destin.moulton@gmail.com>
2017-03-06 16:14:34 -0700

    Reordered tests.

--- test/gopher.test.js
+++ test/gopher.test.js
@@ -2,6 +2,62 @@ var expect = require("chai").expect;
 var gopher = require("../src/gopher.js");
 
 describe("gopher pub/sub", function(){
+    it("is undefined when called directly with no parameter", function(){
+        var alpha_var = "";
+        var alpha = function(message){
+            alpha_var += message;
+        }
+        expect(gopher()).to.equal(undefined);
+    });
+
+    describe("subscribes", function(){
+        it("a single function and unsubscribes that function", function(){
+            var complete_package = {};
+            var funtest = function(packobj){
+                complete_package["packobj"] = packobj;
+            }
+
+            gopher("test").subscribe(funtest);
+
+            var obj_for_transmission = {"foo":"bar"};
+            gopher("test").broadcast(obj_for_transmission);
+
+            expect(complete_package["packobj"]["foo"]).to.equal("bar");
+
+            
+            gopher("test").unsubscribe(funtest);
+            var another_object = {"happy":"narwhal"};
+            gopher("test").broadcast(another_object);
+            expect(complete_package["packobj"]).to.not.have.property("happy");
+        });
+
+        it("multiple functions and then unsubscribes a single function", function(){
+            var CHANNEL = "greek";
+            var alpha_var = "";
+            var alpha = function(message){
+                alpha_var += message;
+            }
+
+            var beta_var = "";
+            var beta = function(message){
+                beta_var += message;
+            }
+            
+            gopher(CHANNEL).subscribe(alpha);
+            gopher(CHANNEL).subscribe(beta);
+
+            gopher(CHANNEL).broadcast("socrates");
+
+            expect(alpha_var).to.equal("socrates");
+            expect(beta_var).to.equal("socrates");
+            
+            gopher(CHANNEL).unsubscribe(alpha);
+            
+            gopher(CHANNEL).broadcast("plato");
+            expect(alpha_var).to.equal("socrates");
+            expect(beta_var).to.equal("socratesplato");
+        });
+    });
     describe("subscribe a single function and broadcast", function(){
         it("an objects", function(){
 
@@ -55,61 +111,6 @@ describe("gopher pub/sub", function(){
         });
     });
 
-    describe("subscribes", function(){
-        it("a single function and unsubscribes that function", function(){
-            var complete_package = {};
-            var funtest = function(packobj){
-                complete_package["packobj"] = packobj;
-            }
-
-            gopher("test").subscribe(funtest);
-
-            var obj_for_transmission = {"foo":"bar"};
-            gopher("test").broadcast(obj_for_transmission);
-
-            expect(complete_package["packobj"]["foo"]).to.equal("bar");
-
-            
-            gopher("test").unsubscribe(funtest);
-            var another_object = {"happy":"narwhal"};
-            gopher("test").broadcast(another_object);
-            expect(complete_package["packobj"]).to.not.have.property("happy");
-        });
-
-        it("multiple functions and then unsubscribes a single function", function(){
-            var alpha_var = "";
-            var alpha = function(message){
-                alpha_var += message;
-            }
-
-            var beta_var = "";
-            var beta = function(message){
-                beta_var += message;
-            }
-            
-            gopher("greek").subscribe(alpha);
-            gopher("greek").subscribe(beta);
-
-            gopher("greek").broadcast("socrates");
-
-            expect(alpha_var).to.equal("socrates");
-            expect(beta_var).to.equal("socrates");
-            
-            gopher("greek").unsubscribe(alpha);
-            
-            gopher("greek").broadcast("plato");
-            expect(alpha_var).to.equal("socrates");
-            expect(beta_var).to.equal("socratesplato");
-        });
-    });
-
     
-    it("undefined when called directly with no parameter", function(){
-        var alpha_var = "";
-        var alpha = function(message){
-            alpha_var += message;
-        }
-        expect(gopher()).to.equal(undefined);
-    });
     
 });
